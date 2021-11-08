dd763812a0a504d3e76639c22f70b98dda1c38de
fawadik <fawadik@yahoo.com>
2021-11-08 18:29:01 +0100

    Bash-Script assignment completed with both the parts

--- initialAssignment.sh
+++ initialAssignment.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-read -p "Please enter the directory" directory
+read -p "Please enter the directory. . . !" directory
 cd
 cd ${directory}
+if [[ $?  -eq 0 ]]; then
 pwd=$(pwd)
-echo "${pwd}"
-echo $(ls)
-for fileName in $(ls); do
+#for fileName in $(ls); do
 
-if [[ $fileName =~ .tsx ]]; then
-$count == $count + 1
-echo "0"
+#if [[ $fileName =~ .tsx ]]; then
+#$count=$[$count+1]
+#echo "0"
+#fi
+#done
+#echo "There are ${count} tsx files"
+file=$(ls *.tsx | wc -l)
+echo "There are $file .tsx files in the directory"
+else
+echo "Invalid Directory"
 fi
-done
-echo "There are ${count} tsx files"
